db6e868319182af74f5055c63a840cc8fd62aba2
Inventor1938 <ivan.kolev@mail.bg>
2017-08-16 07:21:48 +0300

    update clang version
    Change-Id: If6352728d88739042da0111e34ac8afc535b7406

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -1,13 +1,13 @@
 ## Clang/LLVM release versions.
-LLVM_PREBUILTS_VERSION := clang-3859424
+LLVM_PREBUILTS_VERSION := clang-4053586
 LLVM_PREBUILTS_BASE := prebuilts/clang/host
 
 ## RenderScript-specific tools
-RS_LLVM_PREBUILTS_VERSION := clang-3859424
+RS_LLVM_PREBUILTS_VERSION := clang-4053586
 RS_LLVM_PREBUILTS_BASE := prebuilts/clang/host
 
 ## SDCLANG
 SDCLANG := true
-SDCLANG_PATH := vendor/qcom/sdclang-3.8/linux-x86/bin
+SDCLANG_PATH := prebuilts/snapdragon/llvm-3.8/bin
 SDCLANG_LTO_DEFS := build/core/sdllvm-lto-defs.mk
 SDCLANG_COMMON_FLAGS := -O3 -fvectorize-loops
