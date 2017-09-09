c96866763b2a6addff90e32b685a6caf47eb2941
Inventor1938 <ivan.kolev@mail.bg>
2017-09-09 18:43:04 +0300

    Revert "core: combo: armv8-a: specific kryo tuning"
    This reverts commit fa092cc1c128ba5b4a80db989ee45bec2896827d.

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,3 +1,5 @@
-ifneq ($(filter kryo,$(TARGET_2ND_CPU_VARIANT)),)
-	arch_variant_cflags := -mcpu=cortex-a57
+ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+	arch_variant_cflags := -mcpu=cortex-a53
+else
+	arch_variant_cflags :=
 endif
